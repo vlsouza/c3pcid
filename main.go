7aac8c5739c720a65323a8a5d3cb9c5730be7310
vlsouza <viniciuslima76@outlook.com.br>
2021-02-16 23:46:03 +0000

    Fix port

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 
-	port, err := os.Getenv("PORT")
-	if err != nil {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
 		port = "3000"
 	}
 	log.Fatal(http.ListenAndServe(port, router))
